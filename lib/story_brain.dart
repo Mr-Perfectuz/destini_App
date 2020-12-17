898b083d7f0661dd42f391d19be073fae59a942e
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-17 11:04:24 +0500

    Constructor has been set

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -2,8 +2,6 @@ import 'story.dart';
 
 class StoryBrain {}
 
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
-
 List<Story> _storyData = [
   Story(
       storyTitle:
@@ -39,7 +37,9 @@ List<Story> _storyData = [
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
-
+String getStory() {
+  return _storyData[0].storyTitle;
+}
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
