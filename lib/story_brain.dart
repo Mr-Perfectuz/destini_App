a1238e58156b5f9a2acfb31ed8d3cc69a91fe47a
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-18 07:30:26 +0500

    Constructor has been set

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,33 +47,20 @@ class StoryBrain {
   }
 
   void nextStory(int choiceNumber) {
-    switch (storyNumber) {
-      case 0:
-        if (choiceNumber == 1) {
-          storyNumber = 2;
-        } else {
-          storyNumber = 1;
-        }
-        break;
-
-      case 1:
-        if (choiceNumber == 1) {
-          storyNumber = 2;
-        } else {
-          storyNumber = 3;
-        }
-        break;
-
-      case 2:
-        if (choiceNumber == 1) {
-          storyNumber = 5;
-        } else {
-          storyNumber = 4;
-        }
-        break;
-
-      default:
-        restart()
+    if ((choiceNumber == 1) && (storyNumber == 0)) {
+      storyNumber = 2;
+    } else if ((choiceNumber == 2) && (storyNumber == 0)) {
+      storyNumber = 1;
+    } else if ((choiceNumber == 1) && (storyNumber == 1)) {
+      storyNumber = 2;
+    } else if ((choiceNumber == 2) && (storyNumber == 1)) {
+      storyNumber = 3;
+    } else if ((choiceNumber == 2) && (storyNumber == 2)) {
+      storyNumber = 4;
+    } else if ((choiceNumber == 1) && (storyNumber == 2)) {
+      storyNumber = 5;
+    } else if ([3, 4, 5].contains(storyNumber)) {
+      storyNumber = 0;
     }
   }
 }
