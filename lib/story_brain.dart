5153dd0e2615502b48dd8fa497dd83ede11a3078
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-18 09:04:13 +0500

    Constructor has been set

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -63,12 +63,12 @@ class StoryBrain {
       _storyNumber = 0;
     }
   }
+
+  bool buttonShouldBeVisible() {
+    if ((_storyNumber == 0) || (_storyNumber == 1) || (_storyNumber == 2)) {
+      return true;
+    }
+  }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
